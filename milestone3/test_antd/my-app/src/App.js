3363ab796d0e4c04245ee8ce72dbb35b57a3db31
FlazeNaive <603015257@qq.com>
2024-05-24 16:19:57 +0200

    test npm

---
+++ milestone3/test_antd/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
