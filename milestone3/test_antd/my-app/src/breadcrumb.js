cbb267a25c3a38382af380a805de311cdd28717f
Kai-Lien <moolar0978016749@gmail.com>
2024-05-31 15:14:41 +0200

    remove duplicated link names

--- milestone3/test_antd/my-app/src/breadcrumb.js
+++ milestone3/test_antd/my-app/src/breadcrumb.js
@@ -1,3 +1,4 @@
+// Breadcrumbs.js
 import React from 'react';
 import { Breadcrumb } from 'antd';
 import { useLocation, Link } from 'react-router-dom';
@@ -6,16 +7,24 @@ const Breadcrumbs = () => {
   const location = useLocation();
   const pathSnippets = location.pathname.split('/').filter(i => i);
 
+  const breadcrumbNameMap = {
+    '/project-2024-mushrooms': 'Project 2024 Mushrooms',
+    '/project-2024-mushrooms/stack-chart': 'Stack Chart',
+    '/project-2024-mushrooms/pie-chart': 'Pie Chart',
+    '/project-2024-mushrooms/cloud-chart': 'Cloud Chart',
+    '/project-2024-mushrooms/detail': 'Detail',
+    '/project-2024-mushrooms/overview': 'Overview',
+  };
+
   const breadcrumbItems = [
     <Breadcrumb.Item key="home">
-      <Link to="/">HOME</Link>
+      <Link to="/">Home</Link>
     </Breadcrumb.Item>,
     ...pathSnippets.map((_, index) => {
       const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
-      const formattedUrl = url.replace('/', '').replace(/-/g, ' ').toUpperCase();
       return (
         <Breadcrumb.Item key={url}>
-          <Link to={url}>{formattedUrl}</Link>
+          <Link to={url}>{breadcrumbNameMap[url] || url}</Link>
         </Breadcrumb.Item>
       );
     }),
